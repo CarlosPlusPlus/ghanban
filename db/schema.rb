1ea8a346488c6e7d51bdd0a83c4b2aa1a138c283
Adam Waxman <awaxman11@gmail.com>
2015-02-28 17:22:44 -0500

    Add basic Issue model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228195805) do
+ActiveRecord::Schema.define(version: 20150228221813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20150228195805) do
 
   add_index "boards_users", ["board_id"], name: "index_boards_users_on_board_id", using: :btree
   add_index "boards_users", ["user_id"], name: "index_boards_users_on_user_id", using: :btree
+
+  create_table "issues", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "repo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "repos", force: :cascade do |t|
     t.string   "name"
