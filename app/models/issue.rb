2ea460928b18e48cbcc9598a5e59067d36ccd052
Carlos Lazo <carlos.jose.lazo@gmail.com>
2015-03-07 13:05:48 -0500

    Cleans up some code.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,17 +2,14 @@ class Issue < ActiveRecord::Base
   has_and_belongs_to_many :labels
   belongs_to :repo
 
+  ISSUE_CATEGORIES = ['category', 'priority', 'status', 'size', 'team', 'type']
+
   def add_labels(issue, repo)
     if labels = issue[:labels]
       repo_id = repo.id
 
       labels.each do |label|
-        label_type = label[:name].split(":").first
-        if ['category', 'priority', 'status', 'team', 'type', 'size'].include? label_type
-          label_type = "issue_type" if label_type == "type"
-          self[label_type.to_sym] = label[:name].split(":").last.strip
-          self.save
-        end
+        add_custom_attribute(label)
         self.labels << Label.find_or_create_by(
                          :repo_id => repo_id,
                          :name    => label[:name],
@@ -22,4 +19,16 @@ class Issue < ActiveRecord::Base
       end
     end
   end
+
+  private
+    def add_custom_attribute(label)
+      label_type = label[:name].split(':').first
+        if ISSUE_CATEGORIES.include?(label_type)
+          label_type = 'issue_type' if label_type == 'type'
+          # [TODO] CJL // 2015-03-07
+          # Find a way to set attribute not using this notation.
+          self[label_type.to_sym] = label[:name].split(":").last.strip
+        end
+    end
+
 end
