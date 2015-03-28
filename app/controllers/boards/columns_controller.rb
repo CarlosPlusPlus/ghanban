4e7bcdb2b56f35d7bf8d432ee306944b27a0b698
Carlos Lazo <carlos.jose.lazo@gmail.com>
2015-03-28 13:23:40 -0400

    Refactors code in Boards::ColumnsController.

--- app/controllers/boards/columns_controller.rb
+++ app/controllers/boards/columns_controller.rb
@@ -1,43 +1,40 @@
 class Boards::ColumnsController < ApplicationController
+  before_action :find_board, only: [:index, :new, :create]
 
   def index
-    @board = Board.find(params[:board_id])
     @columns = @board.columns
   end
 
   def show
-    
   end
 
   def new
-    @board = Board.find(params[:board_id])
     @column = @board.columns.build
   end
 
   def create
-    @board = Board.find(params[:board_id])
     @column = @board.columns.build(columns_params)
+
     if @column.save
       redirect_to board_columns_path
     end
   end
 
   def edit
-    
   end
 
   def update
-    
   end
 
   def destroy
-    
   end
 
   private
-
     def columns_params
       params.require(:column).permit(:board_id, :label_name)
     end
 
+    def find_board
+      @board = Board.find(params[:board_id])
+    end
 end
